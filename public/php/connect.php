92dc3335e0275f4ecb4b23f6c1f55d79b6855144
mauriziogiuffredi91 <giuffro91@gmail.com>
2021-09-23 18:47:04 +0200

    found error and save

--- public/php/connect.php
+++ public/php/connect.php
@@ -5,26 +5,28 @@ $username = 'root';
 $password = 'root';
 $dbname = 'edu_test';
 
-if (isset($_POST['submit'])) {
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-    $firstName = $_POST['firstName'];
-    $laststName = $_POST['lastName'];
-    $email = $_POST['email'];
-    $eventName = $_POST['eventName'];
-    $eventDesc = $_POST['eventDesc'];
-    $eventDate = $_POST['eventDate'];
-    $eventTime = $_POST['eventTime'];
+$firstName = $_POST['firstName'];
+$laststName = $_POST['lastName'];
+$email = $_POST['email'];
+$eventName = $_POST['eventName'];
+$eventDesc = $_POST['eventDesc'];
+$eventDate = $_POST['eventDate'];
+$eventTime = $_POST['eventTime'];
 
 
-    /*
-    $sql = "INSERT INTO utenti (nome, cognome, email) VALUES
-    ('$firstName', '$laststName', '$email')";
-    */
-    $sql2 = "INSERT INTO eventi (nome_evento, descrizione_evento, data, ora) 
-    VALUES ('$eventName', '$eventDesc', '$eventDate', '$eventTime')";
-    /*
-    mysqli_query($conn, $sql);
-    */
-    mysqli_query($conn, $sql2);
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+//data inside db
+$sql = "INSERT INTO utenti (nome, cognome, email)
+VALUES ('$firstName', '$laststName', '$email')";
+$sql1 = "INSERT INTO eventi (nome_evento, descrizione_evento, date_time, ora)
+VALUES ('$eventName', '$eventDesc', '$eventDate', '$eventTime')";
+
+
+
+$conn->close();
